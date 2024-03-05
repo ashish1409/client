6c35a8dec1b1e3f95d1e1b8f8c32a80e4df195eb
ashish1409 <ashish.choudhary84@gmail.com>
2024-03-06 00:19:40 +0530

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignOut from "./pages/SignOut";
+import SignIn from "./pages/SignIn";
+import NoPage from "./pages/NoPage";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoPage />} />
+        <Route path="/setting" element={<Setting />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
