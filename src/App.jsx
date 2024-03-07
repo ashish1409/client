39a9ef404382fd4742a61a1f3598422656c74a35
ashish1409 <ashish.choudhary84@gmail.com>
2024-03-07 13:00:13 +0530

    resolve header issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import SignOut from "./pages/SignOut";
 import SignIn from "./pages/SignIn";
 import NoPage from "./pages/NoPage";
 import Setting from "./pages/Setting";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
